14dfe2907e0e3f7bdbcee4dea964b03c871460e4
yxj0312 <yanxiajun@hotmail.com>
2020-10-11 14:38:15 +0200

    Fix a factory issue

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,20 +2,31 @@
 
 namespace Tests\Feature;
 
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testBasicTest()
-    {
-        $response = $this->get('/');
+    use DatabaseMigrations;
+    // /**
+    //  * A basic test example.
+    //  *
+    //  * @return void
+    //  */
+    // public function testBasicTest()
+    // {
+    //     $response = $this->get('/');
 
-        $response->assertStatus(200);
+    //     $response->assertStatus(200);
+    // }
+
+    /** @test */
+    function name()
+    {
+        $post = Post::factory()->create();
+        $this->assertFalse($post->isScheduled());
     }
 }
