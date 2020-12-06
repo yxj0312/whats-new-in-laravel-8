6cd46ce912b05a4ce94705b8fb6f2f9cda995d97
yxj0312 <yanxiajun@hotmail.com>
2020-12-06 22:12:45 +0100

    Searing on the frontend

--- lc-ais/src/app.js
+++ lc-ais/src/app.js
@@ -1,6 +1,9 @@
 /* global algoliasearch instantsearch */
 
-const searchClient = algoliasearch('NJEILDQ6NA', 'b05183c8ab3f96cf8f09317bf3dc5e31');
+const searchClient = algoliasearch(
+  'NJEILDQ6NA',
+  'b05183c8ab3f96cf8f09317bf3dc5e31'
+);
 
 const search = instantsearch({
   indexName: 'users',
@@ -23,6 +26,16 @@ search.addWidgets([
   }),
   instantsearch.widgets.pagination({
     container: '#pagination',
+  }),
+  instantsearch.widgets.stats({
+    container: '#stats',
+  }),
+  instantsearch.widgets.hitsPerPage({
+    container: '#hits-per-page',
+    items: [
+      { label: '8 hits per page', value: 8, default: true },
+      { label: '16 hits per page', value: 16 },
+    ],
   }),
 ]);
 
